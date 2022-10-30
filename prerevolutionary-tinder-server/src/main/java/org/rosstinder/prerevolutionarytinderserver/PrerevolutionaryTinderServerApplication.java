693fdd63391c50cf0e96bf150ad3f834f764ce36
selbowgreaser <selbowgreaser@gmail.com>
2022-10-30 16:52:38 +0300

    Init commit

---
+++ prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/PrerevolutionaryTinderServerApplication.java
@@ -0,0 +1,13 @@
+package org.rosstinder.prerevolutionarytinderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrerevolutionaryTinderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrerevolutionaryTinderServerApplication.class, args);
+    }
+
+}
---
+++ prerevolutionary-tinder-server/src/test/java/org/rosstinder/prerevolutionarytinderserver/PrerevolutionaryTinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rosstinder.prerevolutionarytinderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrerevolutionaryTinderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
