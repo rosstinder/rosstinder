fa0c8899e224d937636da56cc0a34ace04b16fa2
Гречищева Софья Руслановна <sgrechishcheva@fil-it.ru>
2022-11-10 13:28:18 +0300

    fix bags after adding db connection

--- prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/model/repository/FavoriteRepository.java
+++ prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/model/repository/FavoriteRepository.java
@@ -6,19 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    //запрос, проверяющий, что данный юзер не лайкал ранее другого юзера
-//    @Query("select isLike from rosstinder.likes where who = :who and whom = :whom")
-//    boolean isUserLikedAnotherUser(@Param("who") UUID who, @Param("whom") UUID whom);
-
-//    //запрос, проверяющий, что лайк данного юзера оказался взаимным
-//    @Query("select isLike from rosstinder.likes where who = :whom and whom = :who")
-//    boolean isUserMatchesWithAnotherUser(@Param("who") UUID who, @Param("whom") UUID whom);
-
-//    //запрос, возвращающий уиды юзеров, к-ых лайкнул конкретный юзер
-//    @Query("select * from rosstinder.likes where id = :who")
-//    List<Like> getAllUserLikes(@Param("id") UUID who);
-
-//    @Query("select * from rosstinder.likes")
-//    List<Like> getAllLikes();
 
 }
