e1e701b85294d6fc59776119025b3d4b53a8e826
selbowgreaser <selbowgreaser@gmail.com>
2022-11-13 13:52:54 +0300

    Deleted extra DTO

--- prerevolutionary-tinder-tgbot-client/src/main/java/org/rosstinder/prerevolutionarytindertgbotclient/model/UserDto.java
+++
@@ -1,8 +0,0 @@
-package org.rosstinder.prerevolutionarytindertgbotclient.model;
-
-public class UserDto {
-    private Long chatId;
-    private String status;
-    private Long lastFavoriteNumber;
-    private Long lastProfileNumber;
-}
