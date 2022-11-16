e9d334ddb95fc8ff54fd1ae6ff54a89fc0265feb
Гречищева Софья Руслановна <sgrechishcheva@fil-it.ru>
2022-11-16 21:47:47 +0300

    форматирование

--- prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/FavoriteService.java
+++ prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/FavoriteService.java
@@ -2,9 +2,14 @@ package org.rosstinder.prerevolutionarytinderserver.service;
 
 public interface FavoriteService {
     void makeLikeOrDislike(Long whoChatId, boolean isLike);
+
     Long findNextFavoriteChatId(Long chatId);
+
     Long findPreviousFavoriteChatId(Long chatId);
+
     String findFavoriteRelation(Long whoChatId);
+
     String findProfileByteString(Long id);
+
     String getMassageIfMatch(Long whoChatId);
 }
--- prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/UserService.java
+++ prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/UserService.java
@@ -5,14 +5,23 @@ import org.rosstinder.prerevolutionarytinderserver.model.entity.User;
 
 public interface UserService {
     User findUserByChatId(Long chatId);
+
     Profile findProfileByChatId(Long chatId);
+
     Profile findProfileById(Long id);
+
     void updateUserStatus(Long chatId, String status);
+
     void createUser(Long chatId, String status);
+
     void createProfile(Long chatId);
+
     String findProfileByteString(Long id);
+
     Long findNextProfileByChatId(Long chatId);
+
     String findNameAndGender(Long id);
+
     void updateUserKeyValue(Long chatId, String key, String value);
 
 }
