b211ddf8e34e3985a2a97860e6fea870b703d8e8
selbowgreaser <selbowgreaser@gmail.com>
2022-11-18 01:14:01 +0300

    Changed TranslatorClientImpl

--- prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/TranslatorClientImpl.java
+++ prerevolutionary-tinder-server/src/main/java/org/rosstinder/prerevolutionarytinderserver/service/TranslatorClientImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class TranslatorClient {
+public class TranslatorClientImpl {
     @Value("${translator.server}")
     private String server;
     RestTemplate restTemplate = new RestTemplate();
