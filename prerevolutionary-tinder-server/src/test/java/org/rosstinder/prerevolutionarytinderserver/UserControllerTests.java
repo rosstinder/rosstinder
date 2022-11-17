2156b63f28b7c6c2e25ec5b203d0c43ee6b5f0fb
Гречищева Софья Руслановна <sgrechishcheva@fil-it.ru>
2022-11-17 19:42:15 +0300

    Исправления по результатам ревью: добавление тестов.

--- prerevolutionary-tinder-server/src/test/java/org/rosstinder/prerevolutionarytinderserver/UserControllerTests.java
+++ prerevolutionary-tinder-server/src/test/java/org/rosstinder/prerevolutionarytinderserver/UserControllerTests.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
